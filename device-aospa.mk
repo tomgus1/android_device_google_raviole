e21058eb59cf1ada8f5f644e77dd32f1ec39d415
Chris Crump <chriscrawford893@gmail.com>
2022-07-07 01:02:59 -0400

    Set camera extension packages
    Change-Id: Ifab68d9710b144e9d2a81b911c770dc941b19635

--- device-aospa.mk
+++ device-aospa.mk
@@ -14,6 +14,11 @@
 # limitations under the License.
 #
 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
